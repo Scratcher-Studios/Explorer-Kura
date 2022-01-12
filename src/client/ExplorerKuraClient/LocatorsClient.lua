d5e142ff348a248f5e6114c0c650002678b46055
EcoScratcher <88910870+EcoScratcher@users.noreply.github.com>
2022-01-12 15:59:13 +1300

    Fixed formatting on LocatorsClient

--- src/client/ExplorerKuraClient/LocatorsClient.lua
+++ src/client/ExplorerKuraClient/LocatorsClient.lua
@@ -2,83 +2,89 @@ local obj = {}
 obj.__index = obj
 local Players = game:GetService("Players")
 local Fusion = require(script.Parent.Fusion)
-local Maid = require(script.Parent.Icon.Maid)
+local CharacterConnections = table.create(Players.MaxPlayers)
+local KuraRE = game:GetService("ReplicatedStorage").KuraRE
 
 function obj.New()
-    local self = table.create(Players.MaxPlayers)
-    self._maid = Maid.new()
-    local function OnPlayerAdded(player)
-        local function AddGui()
-                local rootpart = player.Character:FindFirstChild("HumanoidRootPart")
-                local BillboardGui = Fusion.New "BillboardGui" {
-                    Parent = rootpart;
-                    ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
-                    AlwaysOnTop = true;
-                    ClipsDescendants = true;
-                    LightInfluence = 0;
-                    Size = UDim2.fromOffset(150, 100);
-                    StudsOffsetWorldSpace = Vector3.new(0, 5, 0);
-                    Enabled = false;
-                    [Fusion.Children] = {
-                        Fusion.New "ImageLabel" {
-                            AnchorPoint = Vector2.new(0.5, 0);
-                            BackgroundColor3 = Color3.new(1, 1, 1);
-                            BackgroundTransparency = 1;
-                            Position = UDim2.fromScale(0.5, 0);
-                            Size = UDim2.fromScale(1, 0.75);
-                            Image = "rbxassetid://8127902577";
-                            ScaleType = Enum.ScaleType.Fit;
-                        };
-                        Fusion.New "TextLabel" {
-                            AnchorPoint = Vector2.new(0.5, 1);
-                            BackgroundColor3 = Color3.new(1, 1, 1);
-                            BackgroundTransparency = 1;
-                            Position = UDim2.fromScale(0.5, 1);
-                            Size = UDim2.fromScale(1, 0.25);
-                            Font = Enum.Font.GothamBold;
-                            TextColor3 = Color3.new(1, 1, 1);
-                            TextScaled = true;
-                            TextStrokeTransparency = 0;
-                            Text = player.Name;
-                        }
-                    };
-                }
-                self[player] = BillboardGui
-        end
-        if player.Character then
-            AddGui()
-        end
-    end
-    Players.PlayerAdded:Connect(OnPlayerAdded)
-    for _, player in pairs(Players:GetPlayers()) do
-        OnPlayerAdded(player)
-    end
-    setmetatable(self, obj)
-    return self
+	local self = table.create(Players.MaxPlayers)
+	local function OnPlayerAdded(player)
+		local function AddGui()
+			local rootpart = player.Character:FindFirstChild("HumanoidRootPart")
+			local BillboardGui = Fusion.New "BillboardGui" {
+				Parent = rootpart;
+				ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+				AlwaysOnTop = true;
+				ClipsDescendants = true;
+				LightInfluence = 0;
+				Size = UDim2.fromOffset(150, 100);
+				StudsOffsetWorldSpace = Vector3.new(0, 5, 0);
+				Enabled = false;
+				[Fusion.Children] = {
+					Fusion.New "ImageLabel" {
+						AnchorPoint = Vector2.new(0.5, 0);
+						BackgroundColor3 = Color3.new(1, 1, 1);
+						BackgroundTransparency = 1;
+						Position = UDim2.fromScale(0.5, 0);
+						Size = UDim2.fromScale(1, 0.75);
+						Image = "rbxassetid://8127902577";
+						ScaleType = Enum.ScaleType.Fit;
+					};
+					Fusion.New "TextLabel" {
+						AnchorPoint = Vector2.new(0.5, 1);
+						BackgroundColor3 = Color3.new(1, 1, 1);
+						BackgroundTransparency = 1;
+						Position = UDim2.fromScale(0.5, 1);
+						Size = UDim2.fromScale(1, 0.25);
+						Font = Enum.Font.GothamBold;
+						TextColor3 = Color3.new(1, 1, 1);
+						TextScaled = true;
+						TextStrokeTransparency = 0;
+						Text = player.Name;
+					}
+				};
+			}
+			self[player] = BillboardGui
+		end
+		if player.Character then
+			AddGui()
+		end
+		CharacterConnections[player] = player.CharacterAdded:Connect(AddGui)
+	end
+	Players.PlayerAdded:Connect(OnPlayerAdded)
+	for _, player in pairs(Players:GetPlayers()) do
+		OnPlayerAdded(player)
+	end
+	Players.PlayerRemoving:Connect(function(player)
+		CharacterConnections[player]:Disconnect()
+	end)
+	setmetatable(self, obj)
+	return self
 end
 
 function obj:ShowLocator(player)
-    if self[player] then
-        self[player].Enabled = true
-    else
-        warn("unable to find player billboardgui")
-    end
+	if self[player] then
+		self[player].Enabled = true
+	else
+		warn("unable to find player billboardgui")
+	end
+	KuraRE:FireServer("ShowLocator", {Target = player})
 end
 
 function obj:HideLocator(player)
-    if self[player] then
-        self[player].Enabled = false
-    else
-        warn("unable to find player billboardgui")
-    end
+	if self[player] then
+		self[player].Enabled = false
+	else
+		warn("unable to find player billboardgui")
+	end
+	KuraRE:FireServer("HideLocator", {Target = player})
 end
 
 function obj:GetPlayerLocatorStatus(player)
-    if self[player] then
-        return self[player].Enabled
-    else
-        return
-    end
+	if self[player] then
+		return self[player].Enabled
+	else
+		return
+	end
 end
 
 return obj
